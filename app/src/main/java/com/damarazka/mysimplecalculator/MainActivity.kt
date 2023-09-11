f4892c3dd7ef66b5daafa5c168021fc11334e1b6
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-09-11 08:34:01 +0700

    add fun's every button

--- app/src/main/java/com/damarazka/mysimplecalculator/MainActivity.kt
+++ app/src/main/java/com/damarazka/mysimplecalculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.damarazka.mysimplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onClearClick(view: View) {}
+    fun onBackClick(view: View) {}
+    fun onOperatorClick(view: View) {}
+    fun onEqualClick(view: View) {}
+    fun onAllClearClick(view: View) {}
+    fun onDigitClick(view: View) {}
 }
